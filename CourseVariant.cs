575a9f9b66cca7bcc68879c1c61d5a9998307e10
Martin Kozma <martin@kozmatech.com>
2019-06-27 23:10:49 +1000

    #OD-3 Create course variants class to read file

--- CourseVariant.cs
+++ CourseVariant.cs
@@ -1,21 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace OR_Results
 {
-    public class CourseVariant : CSVHelper<CourseVariant>
+    public class CourseVariant: CSVHelper<CourseVariant>
     {
-        public int Id { get; set; }
+        public int CourseVariantId { get; set; }
         public string CourseId { get; set; }
+        public string Field1 { get; set; }
+        public int Field2 { get; set; }
         public string Field3 { get; set; }
-        public string Field4 { get; set; }
-        public string Field5 { get; set; }
-        public string Field6 { get; set; }
-        public string Field7 { get; set; }
-        public List<int> CourseControls { get; set; }
-
+        public int Field4 { get; set; }
+        public int Field5 { get; set; }
+        public List<string> Controls { get; set; }
     }
 }
