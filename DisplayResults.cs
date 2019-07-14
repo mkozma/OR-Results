fcfa4371160e9f96f72f351c95d7ad981053c8ea
Martin Kozma <martin@kozmatech.com>
2019-07-15 00:57:08 +1000

    put back refresh interval

--- DisplayResults.cs
+++ DisplayResults.cs
@@ -30,7 +30,7 @@ namespace OR_Results
 
             html.Append("<html>");
             html.Append("<head>");
-            //html.Append("<meta http-equiv='refresh' content='5' />");
+            html.Append("<meta http-equiv='refresh' content='5' />");
             html.Append("<Link rel='stylesheet' href='css/bootstrap.min.css'>");
             html.Append("<Link rel='stylesheet' href='css/main.css'>");
 
